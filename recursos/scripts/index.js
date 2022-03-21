c137b132542ff0b7fcbaf48073fb06d4849995ec
Juan Carlos <juanc6v@gmail.com>
2022-03-21 20:25:14 -0300

    js imagenes

--- recursos/scripts/index.js
+++ recursos/scripts/index.js
@@ -1,8 +1,8 @@
 let app = new Vue({
     el: '#app',
     data: {
-        logos: ['../recursos/imagenes/logos/html-5.png',
-            '../recursos/imagenes/logos/css-3.png', '../recursos/imagenes/logos/js.png', '../recursos/imagenes/logos/java.png', '../recursos/imagenes/logos/vue.png', '../recursos/imagenes/logos/spring.png', '../recursos/imagenes/logos/skp.png', '../recursos/imagenes/logos/v-ray.png', '../recursos/imagenes/logos/photoshop.png', '../recursos/imagenes/logos/autocad.png'
+        logos: ['./recursos/imagenes/logos/html-5.png',
+            './recursos/imagenes/logos/css-3.png', './recursos/imagenes/logos/js.png', './recursos/imagenes/logos/java.png', './recursos/imagenes/logos/vue.png', './recursos/imagenes/logos/spring.png', './recursos/imagenes/logos/skp.png', './recursos/imagenes/logos/v-ray.png', './recursos/imagenes/logos/photoshop.png', './recursos/imagenes/logos/autocad.png'
         ],
         arq: false,
         web: false
