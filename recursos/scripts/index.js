873a986de683a52cbdac4e8f2e5de1d86b112844
Juan Carlos <juanc6v@gmail.com>
2022-03-21 20:38:31 -0300

    fix ruta

--- recursos/scripts/index.js
+++ recursos/scripts/index.js
@@ -2,7 +2,7 @@ let app = new Vue({
     el: '#app',
     data: {
         logos: ['recursos/imagenes/logos/html-5.png',
-            'recursos/imagenes/logos/css-3.png', 'recursos/imagenes/logos/js.png', 'recursos/imagenes/logos/java.png', 'recursos/imagenes/logos/vue.png', './recursos/imagenes/logos/spring.png', './recursos/imagenes/logos/skp.png', './recursos/imagenes/logos/v-ray.png', './recursos/imagenes/logos/photoshop.png', './recursos/imagenes/logos/autocad.png'
+            'recursos/imagenes/logos/css-3.png', 'recursos/imagenes/logos/js.png', 'recursos/imagenes/logos/java.png', 'recursos/imagenes/logos/vue.png', 'recursos/imagenes/logos/spring.png', 'recursos/imagenes/logos/skp.png', 'recursos/imagenes/logos/v-ray.png', 'recursos/imagenes/logos/photoshop.png', 'recursos/imagenes/logos/autocad.png'
         ],
         arq: false,
         web: false
