599586104b87ffcb84f5a43aa0dbbb74990713c9
Juan Carlos <juanc6v@gmail.com>
2022-03-22 17:18:02 -0300

    info proyectos

--- recursos/scripts/index.js
+++ recursos/scripts/index.js
@@ -5,9 +5,14 @@ let app = new Vue({
             'recursos/imagenes/logos/css-3.png', 'recursos/imagenes/logos/js.png', 'recursos/imagenes/logos/java.png', 'recursos/imagenes/logos/vue.png', 'recursos/imagenes/logos/spring.png', 'recursos/imagenes/logos/skp.png', 'recursos/imagenes/logos/v-ray.png', 'recursos/imagenes/logos/photoshop.png', 'recursos/imagenes/logos/autocad.png'
         ],
         arq: false,
-        web: false
+        web: false,
+        banco: false,
+        hockey: false,
+        senado: false,
+        danza: false,
     },
     mounted() {
+        body = document.querySelector('.contenedor-total')
         btnWeb = document.querySelector('.botton-web');
         btnArq = document.querySelector('.botton-arq');
     },
@@ -25,6 +30,39 @@ let app = new Vue({
                 btnWeb.classList.add('tachado')
                 btnArq.classList.remove('tachado')
             }
+        },
+        abrirProyecto(proyecto) {
+            switch (proyecto) {
+                case 'hockey':
+                    this.cerrarProyecto();
+                    body.classList.add('desenfocar');
+                    this.hockey = true;
+                    break;
+                case 'congreso':
+                    this.cerrarProyecto();
+                    body.classList.add('desenfocar');
+                    this.senado = true;
+                    break;
+                case 'banco':
+                    this.cerrarProyecto();
+                    body.classList.add('desenfocar');
+                    this.banco = true;
+                    break;
+                case 'danza':
+                    this.cerrarProyecto();
+                    body.classList.add('desenfocar');
+                    this.danza = true;
+                    break;
+                default:
+                    break;
+            }
+        },
+        cerrarProyecto() {
+            body.classList.remove('desenfocar');
+            this.hockey = false;
+            this.senado = false;
+            this.banco = false;
+            this.danza = false;
         }
     },
 })
