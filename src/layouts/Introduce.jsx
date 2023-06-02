b54aca6372941055c8d3055f0ae539ba5a400665
Juan C. Vilcherez <juanc.vilcherrez@gmail.com>
2023-06-02 01:17:11 -0300

    layout introduction

--- src/layouts/Introduce.jsx
+++ src/layouts/Introduce.jsx
@@ -2,16 +2,24 @@ const Introduce = () => {
     let gretting = "<>HOLA MUNDO</>"
     return (
         <>
-            <div >
-                <div>
-                    <p className="text-light"> {gretting} </p>
-                    <p className="title">Me llamo Juan Carlos</p>
-                    <p className="title">Desarrollador Full Stack</p>
-                    <button>CONTACTAME</button>
-                </div>
-                <div className="avatar">
+            <div className="container">
+                <nav>
+                    <button type="button">X </button>
+                    <button type="button">ES</button>
+                </nav>
 
+                <div className="container-me">
+                    <div>
+                        <p className="text-light"> {gretting} </p>
+                        <p className="title">Me llamo Juan Carlos</p>
+                        <p className="title">Desarrollador Full Stack</p>
+                        <button className="btn-contact" type="button">CONTACTAME</button>
+                    </div>
+                    <div className="avatar">
+
+                    </div>
                 </div>
+
                 <div className="footer">
                     <p className="text-footer">juanc.vilcherrez@gmail.com</p>
                     <div>
@@ -20,14 +28,50 @@ const Introduce = () => {
                     </div>
                 </div>
                 <style jsx>{`
-                
+                nav{
+                    display:flex;
+                    justify-content:space-between;
+                    align-items:center;
+                    color:var(--white);
+                }
+                nav>button{
+                    color:var(--black-light)
+                }
+                .btn-contact{                  
+                    border: solid var(--white);
+                }
+                .text-light{
+                    margin-bottom:1rem !important
+                }
                 .footer{
                     display:flex;
-                    justify-content:space-between   
+                    justify-content:space-between;
+                    padding-bottom:10px
                 }
                 .footer>div{
                     display:flex;
                     justify-content:space-between;
+                    width:20%
+                }
+                .container{
+                    display:flex;
+                    flex-direction:column;
+                    justify-content: space-between;
+                    height:100vh;
+                    padding: 0rem 100px 0rem 100px;
+                }
+                .container-me{
+                    display:flex;
+                    justify-content:space-between;
+                    flex-wrap:wrap
+                }
+                .avatar{
+                    width: 150px;
+                    height: 150px;
+                    -moz-border-radius: 50%;
+                    -webkit-border-radius: 50%;
+                    border-radius: 50%;
+                    background: var(--black-light);
                 }
                 `}</style>
             </div>
