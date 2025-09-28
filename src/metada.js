33f42717a7a65efcddd960f40d4c12439f440eff
juancarlos2v <juanc.vilcherrez@gmail.com>
2025-09-28 04:05:26 -0300

    🔍️  implementacion de seo

---
+++ src/metada.js
@@ -0,0 +1,22 @@
+export const metadata = {
+    title: "Juan Carlos Vilcherrez | Desarrollador",
+    description: "Portfolio de Juan Carlos Vilcherrez, desarrollador Java y React.",
+    keywords: ["Vilcherrez", "desarrollador", "Java", "React", "Next.js", "portfolio", "tecnologia", "tech", "tecba"],
+    authors: [{ name: "Juan Carlos Vilcherrez" }],
+    openGraph: {
+        title: "Juan Carlos Vilcherrez | Desarrollador",
+        description: "Portfolio de proyectos y experiencia en Java, Spring y React.",
+        url: "https://juanvilcherrez.vercel.app",
+        siteName: "Portfolio de Vilcherrez",
+        images: [
+            {
+                url: "https://juanvilcherrez.vercel.app/preview.png",
+                width: 1200,
+                height: 630,
+                alt: "Portfolio de Vilcherrez"
+            },
+        ],
+        locale: "es_ES",
+        type: "website",
+    },
+};
