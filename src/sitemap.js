fa10d51259cb7361595976fbb44c05fd768ad731
juancarlos2v <juanc.vilcherrez@gmail.com>
2025-09-28 04:17:04 -0300

    🔍️  sitemap + robots

---
+++ src/sitemap.js
@@ -0,0 +1,11 @@
+// app/sitemap.js
+export default function sitemap() {
+    return [
+        {
+            url: 'https://juanvilcherrez.vercel.app',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1.0,
+        }
+    ];
+}
